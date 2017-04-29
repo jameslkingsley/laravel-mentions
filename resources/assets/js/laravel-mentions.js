05a560ffffba1b6360284836263b82e937b2c48e
James Kingsley <jameslkingsley@outlook.com>
2017-04-29 23:34:01 +0100

    Fix triggers

--- resources/assets/js/laravel-mentions.js
+++ resources/assets/js/laravel-mentions.js
@@ -2,6 +2,32 @@ function Mentions(options) {
     var node = document.querySelector(options.input);
     var collections = [];
 
+    var selectTemplate = function(pool) {
+        return function(item) {
+            return '<span class="mention-node" data-object="'
+                + pool.pool + ':' + item.original[pool.reference] + '">'
+                + (pool.trigger || '@')
+                + item.original[pool.display] + '</span>';
+        }
+    }
+
+    var values = function(pool) {
+        return function(text, callback) {
+            if (text.length <= 1) return;
+
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    callback(JSON.parse(this.responseText));
+                }
+            };
+
+            xhttp.open('post', '/api/mentions?p=' + pool.pool + '&q=' + text, true);
+            xhttp.send();
+        }
+    }
+
     for (var i = 0; i < options.pools.length; i++) {
         var pool = options.pools[i];
 
@@ -9,26 +35,8 @@ function Mentions(options) {
             trigger: pool.trigger || '@',
             lookup: pool.display,
             allowSpaces: true,
-            selectTemplate: function(item) {
-                return '<span class="mention-node" data-object="'
-                    + pool.pool + ':' + item.original[pool.reference] + '">'
-                    + (pool.trigger || '@')
-                    + item.original[pool.display] + '</span>';
-            },
-            values: function(text, callback) {
-                if (text.length <= 1) return;
-
-                var xhttp = new XMLHttpRequest();
-
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        callback(JSON.parse(this.responseText));
-                    }
-                };
-
-                xhttp.open('post', '/api/mentions?p=' + pool.pool + '&q=' + text, true);
-                xhttp.send();
-            }
+            selectTemplate: selectTemplate(pool),
+            values: values(pool)
         });
     }
 
